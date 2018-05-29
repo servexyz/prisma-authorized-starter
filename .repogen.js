b8a9a1e3f83de5030a4e0135649098bd44721570
alechp <me@alechp.com>
2018-05-29 16:48:17 -0700

    Added aws-sls-auther module

--- .repogen.js
+++ .repogen.js
@@ -2,7 +2,8 @@ const config = {
   provider: "alechp",
   repospacePath: __dirname, 
   repositories: [
-    { servexyz: "aws-sls-auth-starter" } 
+    { servexyz: "aws-sls-auth-starter" },
+    { servexyz: "aws-sls-auther" }
   ]
 }
 
